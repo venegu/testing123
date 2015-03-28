b95646d8256fbcc780a21ee5464fb5822dbbdc06
venegu <venegu@gmail.com>
2015-03-28 19:37:24 -0400

    set up inserting into mongodb

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-/* Small test */ 
+/* Small test to check nodemon works */ 
 /*console.log('Worked');*/
 
 /* Importing necessary technology */ 
-var mongo = require('mongoose').MongoClient, client = require('socket.io').listen(8080).sockets; 
-/* to be tested */ 
+var mongo = require('mongodb').MongoClient;
+var client = require('socket.io').listen(8080).sockets; 
+
+/* server & callback */
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
+	if(err) throw err; 
+	/* connecting and a closure: whatever needs to be done after client is connected */
+	client.on('connection', function(socket){
+		var col = db.collection('messages'); 
+
+		/* waiting for input */ 
+		socket.on('input', function(data){
+			/*console.log(data);*/
+			/* grabbing data */
+			var name = data.name;
+			var message = data.message;
+
+			/* Inserting into mongo database */ 
+			col.insert({name:name, message:message}, function(){
+				console.log('inserted'); /* just to see if it worked */
+			}); /* end insert */ 
+		}); /* end socket */ 
+	});  /* end connection */ 
+}); /* end mongo.connect */ 
