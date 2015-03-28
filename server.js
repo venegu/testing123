18d9c2e476ff7569aaed01696d71cebe63006228
venegu <venegu@gmail.com>
2015-03-27 23:03:26 -0400

    adding required technology to server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('Worked');
+/* Small test */ 
+/*console.log('Worked');*/
+
+/* Importing necessary technology */ 
+var mongo = require('mongoose').MongoClient, client = require('socket.io').listen(8080).sockets; 
+/* to be tested */ 
